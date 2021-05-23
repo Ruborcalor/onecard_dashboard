a47324f597428598d78f8d39e1110d72a9f7514b
Cole Killian <g.cole.killian@gmail.com>
2021-05-22 20:06:07 -0400

    fix: put api back the way it should be

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 const axiosInstance = axios.create({
   baseURL:
     process.env.NODE_ENV == 'development'
-      ? 'https://api.mcgillonecard.com:2999/'
+      ? 'http://localhost:5000/'
       : 'https://api.mcgillonecard.com:2999/',
 })
 
